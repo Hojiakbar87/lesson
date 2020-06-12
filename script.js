b2205fd28fee4961bb6c22aca6a86a2efc9ae76d
Hojiakbar <max953114@gmail.com>
2020-06-12 07:34:56 +0500

    Lesson

---
+++ script.js
@@ -0,0 +1,27 @@
+let money = prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+
+let appData = {
+    budjet: money,
+    TimeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = +prompt("Во сколько обойдется?", "");
+    if (typeof (a) === "string" && typeof (b) === "string" && a !== null && b !== null && a !== "" && b !== null && a.length > 50){
+        console.log("done");
+        appData[a] = b;
+    }else{
+    }
+}
+
+appData.moneyPerDay = appData.budjet /30;
+
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
