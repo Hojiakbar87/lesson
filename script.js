68a73d7a46cb1f4714c0e69e52fefb282c0350e0
Hojiakbar <max953114@gmail.com>
2020-06-13 08:44:03 +0500

    script

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-let money = prompt("Ваш бюджет на месяц?");
-let time = prompt("Введите дату в формате YYYY-MM-DD");
+
+let money, time;
+
+function start() {
+    money = +prompt("Ваш бюджет на месяц?", "");
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
+    while (true) {
+        if (isNaN(money) || money === "" || money === null) {
+            money = +prompt("Ваш бюджет на месяц?", "");
+            continue;
+        }
+        break;
+    }
+}
+
+start();
 
 let appData = {
     budjet: money,
@@ -10,17 +24,30 @@ let appData = {
     savings: false
 };
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-        b = +prompt("Во сколько обойдется?", "");
-    if (typeof (a) === "string" && typeof (b) === "string" && a !== null && b !== null && a !== "" && b !== null && a.length > 50){
-        console.log("done");
-        appData[a] = b;
-    }else{
+function chooseOptExpenses() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+            b = +prompt("Во сколько обойдется?", "");
+        if (typeof (a) === "string" && typeof (b) === "string" && a !== null && b != null && a !== "" && b != null && a.length > 50) {
+            console.log("done");
+            appData[a] = b;
+        } else {
+        }
     }
 }
 
-appData.moneyPerDay = appData.budjet /30;
+chooseOptExpenses();
+
+
+(appData.moneyPerDay = appData.budjet / 30).toFixed(1);
 
 
 alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уровен доставка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средный уровен доставка");
+} else {
+    console.log("Призошла ошибка");
+}
